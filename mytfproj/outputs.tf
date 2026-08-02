6985c526b8789ada9c351cc8176a374725be334e
mjafferkhader <56647037+mjafferkhader@users.noreply.github.com>
2026-08-02 07:25:50 +0530

    Refactor output to use instance variable

--- mytfproj/outputs.tf
+++ mytfproj/outputs.tf
@@ -1,9 +1,9 @@
 output "instances" {
   value = [
     for instance in aws_instance.mjsinst : {
-      id = aws_instance.mjsinst.id
-      name = aws_instance.mjsinst.tags["Name"]
-      pubip = aws_instance.mjsinst.public_ip
+      id = instance.id
+      name = instance.tags["Name"]
+      pubip = instance.public_ip.id
     }
   ]
 }
