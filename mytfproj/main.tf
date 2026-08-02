8410d246b36039d0e1de58b77154a7526b5d6cb7
mjafferkhader <56647037+mjafferkhader@users.noreply.github.com>
2026-08-02 07:01:24 +0530

    tfchange

--- mytfproj/main.tf
+++ mytfproj/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "mjsinst" {
+  count = terraform.workspace == "prod" ? 3 : 1
   ami           = data.aws_ami.mjs_ami.id
-  instance_type = "t2.micro"
+  instance_type = terraform.workspace == "prod" ? "t3.micro" : "t2.micro"
 
   tags = {
-    Name = "${terraform.workspace}-app"
+    Name = terraform.workspace == "prod" ? "prod-app${count.index + 1}" : "${terraform.workspace}-app"
     Environment = terraform.workspace
   }
 }
